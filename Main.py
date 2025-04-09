56fc9a669fcbb0149be9ee0bc4eeae24b6be7817
MichaelK17 <tatehaya16@gmail.com>
2025-04-09 00:53:38 -0400

    first commit

---
+++ Main.py
@@ -0,0 +1,115 @@
+from datamodel import OrderDepth, UserId, TradingState, Order
+from typing import List
+import string
+
+"""
+Website: https://imc-prosperity.notion.site/Writing-an-Algorithm-in-Python-19ee8453a0938114a15eca1124bf28a1
+
+Description: This is a sample algorithm for the IMC Trading competition.
+
+"""
+
+class Trader:
+    
+    def run(self, state: TradingState):
+        print("traderData: " + state.traderData)
+        print("Observations: " + str(state.observations))
+
+				# Orders to be placed on exchange matching engine
+        result = {}
+        for product in state.order_depths:
+            order_depth: OrderDepth = state.order_depths[product]
+            orders: List[Order] = []
+            acceptable_price = 10  # Participant should calculate this value
+            print("Acceptable price : " + str(acceptable_price))
+            print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
+    
+            if len(order_depth.sell_orders) != 0:
+                best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
+                if int(best_ask) < acceptable_price:
+                    print("BUY", str(-best_ask_amount) + "x", best_ask)
+                    orders.append(Order(product, best_ask, -best_ask_amount))
+    
+            if len(order_depth.buy_orders) != 0:
+                best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
+                if int(best_bid) > acceptable_price:
+                    print("SELL", str(best_bid_amount) + "x", best_bid)
+                    orders.append(Order(product, best_bid, -best_bid_amount))
+            
+            result[product] = orders
+    
+		    # String value holding Trader state data required. 
+				# It will be delivered as TradingState.traderData on next execution.
+        traderData = "SAMPLE" 
+        
+				# Sample conversion request. Check more details below. 
+        conversions = 1
+        return result, conversions, traderData
+
+
+'''
+Tradingstate class holds all the important market information that an algorithm needs to make decisions about which orders to send. 
+'''
+Time = int
+Symbol = str
+Product = str
+Position = int
+
+class TradingState(object):
+    # Constructor for the TradingState class
+    def __init__(self,
+                 traderData: str,
+                 timestamp: Time,
+                 listings: Dict[Symbol, Listing],
+                 order_depths: Dict[Symbol, OrderDepth],
+                 # own_trades: the trades the algorithm itself has done since the last TradingState came in
+                 own_trades: Dict[Symbol, List[Trade]],
+                 # market_trades: the trades that other market participants have done since the last TradingState came in
+                 market_trades: Dict[Symbol, List[Trade]],
+                 # the long or short position that the player holds in every tradable product, in dictionary
+                 position: Dict[Product, Position],
+                 observations: Observation):
+        self.traderData = traderData
+        self.timestamp = timestamp
+        self.listings = listings
+        # order_depths: all the buy and sell orders per product that other market participants have sent and that the algorithm is able to trade with.
+        self.order_depths = order_depths
+        self.own_trades = own_trades
+        self.market_trades = market_trades
+        self.position = position
+        self.observations = observations
+    
+    # toJSON method to convert the TradingState object to a JSON string
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True)
+    
+'''
+Trade class holds the information about a trade that has been executed.
+
+Both the own_trades property and the market_trades property provide the traders with a list of trades per products. Every individual trade in each of these lists is an instance of the Trade class.
+'''
+Symbol = str
+UserId = str
+
+class Trade:
+    # Constructor for the Trade class
+    def __init__(self, 
+                 symbol: Symbol, 
+                 price: int, 
+                 quantity: int, 
+                 buyer: UserId = None, 
+                 seller: UserId = None, 
+                 timestamp: int = 0) -> None:
+        self.symbol = symbol
+        self.price: int = price
+        self.quantity: int = quantity
+        self.buyer = buyer
+        self.seller = seller
+        self.timestamp = timestamp
+
+    # toString method to convert the Trade object to a string
+    def __str__(self) -> str:
+        return "(" + self.symbol + ", " + self.buyer + " << " + self.seller + ", " + str(self.price) + ", " + str(self.quantity) + ", " + str(self.timestamp) + ")"
+
+    def __repr__(self) -> str:
+        return "(" + self.symbol + ", " + self.buyer + " << " + self.seller + ", " + str(self.price) + ", " + str(self.quantity) + ", " + str(self.timestamp) + ")" + self.symbol + ", " + self.buyer + " << " + self.seller + ", " + str(self.price) + ", " + str(self.quantity) + ")"
