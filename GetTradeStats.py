375d680974e2a113e8e4a6e03184294ddeb2b0ca
MichaelK17 <tatehaya16@gmail.com>
2025-04-09 16:18:52 -0400

    commit

---
+++ GetTradeStats.py
@@ -0,0 +1,13 @@
+"""
+This code obtains stas needed for the Main.py.
+We need the following:
+    average
+    standard deviation
+"""
+
+
+def getAverage():
+    int result = 0;
+    return result
+
+def getStandardDeviation():
